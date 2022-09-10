3b9090859a8a3f66564d464e09078da746facbff
jaber4770 <jaber.ahmed4770@gmail.com>
2022-09-10 20:41:42 +0600

    Day_2 start

---
+++ Day_2.js
@@ -0,0 +1,15 @@
+// array
+
+let numberArray = [23, 34, 545, 65, 65, 76, 767, 67, 6, 7, 67];
+let stringArray = ["moto", "patlu", "khasitaram", "chinggam", "Dr.Jatka"];
+console.log(numberArray);
+// check how long the array by length
+console.log(numberArray.length);
+// showing the array on the terminal
+console.log(stringArray);
+console.log(stringArray.length);
+// check index by position;        index start with zero, 0;
+console.log(numberArray[0]);
+
+// find element by searching the element with indexOf();
+console.log(numberArray.indexOf(67));
