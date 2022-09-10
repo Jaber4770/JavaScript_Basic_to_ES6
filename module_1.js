15d3802fe35d12bdeab1aff490c6cf62ca71458f
jaber4770 <jaber.ahmed4770@gmail.com>
2022-09-10 11:30:56 +0600

    day-1 of javascript almost done

---
+++ module_1.js
@@ -0,0 +1,80 @@
+// addition
+var eggPrice = 20;
+var onionPrice = 20;
+let totalPrice = eggPrice + onionPrice;
+console.log(totalPrice);
+
+
+// subtraction
+let fishPrice = 500;
+let myMoney = 600;
+let backMoney = myMoney - fishPrice;
+console.log(backMoney);
+
+
+// divided
+let totalTk = 1000;
+let player = 11;
+let everyOneGet = totalTk / player;
+console.log(everyOneGet);
+
+
+// multiplication
+let oneMangoPrice = 15;
+let mangoQuantity = 24;
+let totalMangoPrice = oneMangoPrice * mangoQuantity;
+console.log(totalMangoPrice);
+
+
+// short hand
+
+// addition
+console.log("addition");
+let price1 = 40;
+console.log(price1);
+price1 = price1 + 10;
+console.log(price1);
+price1 += 5;
+console.log(price1);
+
+// subtraction
+console.log("subtraction");
+let price2 = 30;
+console.log(price2);
+price2 = price2 - 1;
+console.log(price2);
+price2 += 1;
+console.log(price2);
+
+// multiplication 
+console.log("multiplication");
+let price3 = 20;
+console.log(price3);
+price3 = price3 * 2;
+console.log(price3);
+price3 *= 2;
+console.log(price3);
+
+// divition
+console.log("divition");
+let price4 = 10;
+console.log(price4);
+price4 = price4 * 2;
+console.log(price4);
+price4 *= 2;
+console.log(price4);
+
+
+// increment
+console.log("increment");
+let price5 = 5;
+console.log(price5);
+price5++;
+console.log(price5);
+
+// decremental
+console.log("decremental");
+let price6 = 6;
+console.log(price6);
+price6--;
+console.log(price6);
