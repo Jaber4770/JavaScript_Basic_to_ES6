f338a0e6557d9c8e5033badba29a0ca49cb95a47
jaber4770 <jaber.ahmed4770@gmail.com>
2022-09-12 01:29:38 +0600

    even number and odd number showed by while loop

--- Day_3.js
+++ Day_3.js
@@ -10,27 +10,53 @@ console.log(arry);
 arry.pop();
 arry.shift();
 arry.unshift("halka mote suiye poren.");
-if(4>2){
+if (4 > 2) {
     console.log("its false");
 }
-else{
+else {
     console.log("it's true");
 }
 
 // Loop_______________Loop___________Loop_______________Loop___________Loop_______________Loop___________Loop_______________Loop___________
 
 
+
 // while loop
 
 let roastGiven = 0;
-while(roastGiven<8){
+while (roastGiven < 8) {
     console.log("mama roast den please.", roastGiven);
     roastGiven++
 }
 
 // salad serving loop
 let saladGiving = 0;
-while(saladGiving < 5){
+while (saladGiving < 5) {
     console.log("Mama salad dia jan.", saladGiving);
     saladGiving++;
+}
+
+
+// showing 1-10 numbers
+
+let startNumber = 1;
+while (startNumber <= 10) {
+    console.log(startNumber);
+    startNumber++;
+}
+
+
+// showing even number by loop
+
+let evenNumber = 0;
+while (evenNumber <= 10) {
+    console.log("your even number is: ", evenNumber);
+    evenNumber += 2;
+}
+
+// showing odd number by loop
+let oddNumber = 1;
+while (oddNumber <= 10) {
+    console.log("your odd number is: ", oddNumber);
+    oddNumber += 2;
 }
