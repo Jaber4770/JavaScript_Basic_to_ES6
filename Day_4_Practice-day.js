29d6dad7902238f3f1cd45620c1c26aa0ca78f04
jaber4770 <jaber.ahmed4770@gmail.com>
2022-09-15 00:28:44 +0600

    short hand oparation done

--- Day_4_Practice-day.js
+++ Day_4_Practice-day.js
@@ -3,3 +3,39 @@
 var variable1 = 5;
 let variable2 = "this is a string";
 const variable3 = true;
+
+variable1 = "variable valued changed to this text";
+console.log(variable1);
+
+// oparation________________________+, -, *, /, %_____________________________:
+
+
+let oparationNum1 = 5;
+let oparationNum2 = 4;
+console.log("addition of two oparationNum is: ", oparationNum1 + oparationNum2);
+console.log("subtractin of two oparationNum is: ", oparationNum1 - oparationNum2);
+console.log("multiplication of two oparationNum is: ", oparationNum1 * oparationNum2);
+console.log("division of two oparationNum is: ", oparationNum1 / oparationNum2);
+console.log("remainder of two oparationNum is: ", oparationNum1 % oparationNum2);
+
+
+// shorthand_________________+=, -=, *=, /=______________________________________:
+
+let price = 5;
+price += 4;
+console.log(price);
+
+let price2 = 4;
+price2 -= 3;
+console.log(price2);
+
+let price3 = 9;
+price3 *= 2;
+console.log(price3);
+
+let price4 = 6;
+price4 /= 2;
+console.log(price4);
+
+// incremental and decremental_________________________--_______++__________:
+
