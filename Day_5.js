32e75bb85386bc84766edbc4404427a80b810f6f
jaber4770 <jaber.ahmed4770@gmail.com>
2022-09-16 23:16:07 +0600

    function and object work started

--- Day_5.js
+++ Day_5.js
@@ -57,8 +57,62 @@ function sum(a, b, c, d, e) {
 sum(2, 3, 4, 5, 6);
 
 
+// function with return_____________________________________:
+
+function numberAddition(number1, number2){
+    console.log("perameters are: ", number1, "and", number2);
+    let sum = number1 + number2;
+    return sum;
+}
+
+
+let num1 = 10;
+let num2 = 10;
+let total = numberAddition(num1, num2);
+console.log("addition of the function: ",total);
+
+// object____________________________________________:
+
+
+let myPhone = {
+    brand: 'samsung',
+    color: 'blue',
+    storage: '64gb',
+    ram: '6gb',
+    rearCamera: '64MP',
+    frontCamera: '32MP',
+    battery: '6000mAh'
+}
+
+
+console.log(myPhone);
+console.log(myPhone.color);
 
 
 
+
+let myPc = {
+    processor: "i9",
+    hdd: "2TB",
+    ram: "8gb",
+    price: "21k"
+}
+
+console.log(myPc);
+myPc.hdd = "2.240TB"
+console.log(myPc);
+
+
+// another object______________________________________________:
+
+let phone= {
+    brand: "samsung",
+    color: "black",
+    ram: "6gb",
+    storage: "64gb"
+}
+console.log("phone object: ", phone);
+phone.color = "blue";
+console.log("new object: ", phone);
 
 
