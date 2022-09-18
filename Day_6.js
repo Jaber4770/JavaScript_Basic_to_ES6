66bb036d04e037cfbdd17bb3bb4f386a86b481a3
jaber4770 <jaber.ahmed4770@gmail.com>
2022-09-19 00:49:14 +0600

    inch to feet convertion done

---
+++ Day_6.js
@@ -0,0 +1,31 @@
+// Today we will apply all the conecpt we learned:
+
+
+// inch to feet convert calculation:
+
+function inchToFeet(inch){
+    let convertToFeet = inch / 12;
+    return convertToFeet;
+}
+
+let sendingInch = 45;
+let getFeetResult = inchToFeet(sendingInch);
+console.log(getFeetResult, "feet");
+
+
+let sendingInch2 = 299;
+let getFeetResult2 = inchToFeet(sendingInch2);
+console.log(getFeetResult2, "feet");
+
+
+
+// Convertion of Miles to Kilo Meter_____________________________:
+
+
+
+
+
+
+
+
+
