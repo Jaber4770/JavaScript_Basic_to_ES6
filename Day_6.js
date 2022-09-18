2c9ca52a91afa9a5ac672ede620109a2f10fa583
jaber4770 <jaber.ahmed4770@gmail.com>
2022-09-19 01:00:09 +0600

    few convertindone

--- Day_6.js
+++ Day_6.js
@@ -21,9 +21,28 @@ console.log(getFeetResult2, "feet");
 
 // Convertion of Miles to Kilo Meter_____________________________:
 
+function milesToKm(miles){
+    let getKm = miles /1.60934;
+    return getKm;
+}
 
 
+let sendingMiles = 234;
+let getKmResult = milesToKm(sendingMiles);
+console.log(getKmResult, "KM");
 
+
+// Gram to KG convertion___________________________:
+
+function GramToKG(gram){
+    let getKgResult = gram/1000;
+    return getKgResult;
+}
+
+
+let sendingGram = 3434324;
+let KgResult = GramToKG(sendingGram);
+console.log(KgResult, "KG");
 
 
 
