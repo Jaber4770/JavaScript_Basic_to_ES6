8c808a6a3407e463095581f8f3770cf23cfb6bea
jaber4770 <jaber.ahmed4770@gmail.com>
2022-09-19 14:53:16 +0600

    basick leap year checking done

--- Day_6.js
+++ Day_6.js
@@ -3,7 +3,7 @@
 
 // inch to feet convert calculation:
 
-function inchToFeet(inch){
+function inchToFeet(inch) {
     let convertToFeet = inch / 12;
     return convertToFeet;
 }
@@ -21,8 +21,8 @@ console.log(getFeetResult2, "feet");
 
 // Convertion of Miles to Kilo Meter_____________________________:
 
-function milesToKm(miles){
-    let getKm = miles /1.60934;
+function milesToKm(miles) {
+    let getKm = miles / 1.60934;
     return getKm;
 }
 
@@ -34,8 +34,8 @@ console.log(getKmResult, "KM");
 
 // Gram to KG convertion___________________________:
 
-function GramToKG(gram){
-    let getKgResult = gram/1000;
+function GramToKG(gram) {
+    let getKgResult = gram / 1000;
     return getKgResult;
 }
 
@@ -46,5 +46,35 @@ console.log(KgResult, "KG");
 
 
 
+// checking a number it is even or add________________:
+
+function isEven(checkNumber) {
+    if ((checkNumber % 2) == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 
+let evenCheckingNumber = 46;
+let getEvenResult = isEven(evenCheckingNumber);
+console.log("your number is even: ", getEvenResult);
+
+
+// checking leap year__________________________________:
+
+function isLeapYear(year) {
+    let remainder = year % 2;
+    if (remainder === 0) {
+        return ("your Year is leap year.");
+    }
+
+    return ("your Year is not leap year.");
+}
+
+
+let checkingYear = 2020;
+let leapYearResult = isLeapYear(checkingYear);
+console.log(leapYearResult);
