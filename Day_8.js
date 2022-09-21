0d971a4cf1f6548d042e62e52da3092c46a129a4
jaber4770 <jaber.ahmed4770@gmail.com>
2022-09-21 12:01:43 +0600

    by includes and lowecase we can find any text from an string

--- Day_8.js
+++ Day_8.js
@@ -14,6 +14,22 @@ console.log(fullName[2]);
 // we can not push to a string. string index look like array. but it is not array.
 
 
-
-
 console.log(fullName);
+
+console.log(fullName.toLowerCase());
+console.log(fullName.toUpperCase());
+console.log(fullName.toLocaleLowerCase());
+
+
+let lyric = "Tomi bondo kala pakhi ami jno ki. boshonto kale tomay bolte pari ni. sada sada kala kalk."
+console.log(lyric.includes('bondo'));
+
+let lyricLowerCase = lyric.toLowerCase();
+console.log(lyricLowerCase.includes("tomi"));
+
+
+let searchString = "BoNDo";
+let searchLowerCase = searchString.toLowerCase();
+let getResult = lyricLowerCase.includes(searchLowerCase);
+console.log("your search result does exist: ", getResult);
+
