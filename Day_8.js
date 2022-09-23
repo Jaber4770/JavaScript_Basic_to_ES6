c511fa0467aaa44839e23c4808234d66c7585613
jaber4770 <jaber.ahmed4770@gmail.com>
2022-09-23 20:54:20 +0600

    working on day - 8

--- Day_8.js
+++ Day_8.js
@@ -33,3 +33,56 @@ let searchLowerCase = searchString.toLowerCase();
 let getResult = lyricLowerCase.includes(searchLowerCase);
 console.log("your search result does exist: ", getResult);
 
+// _______________________________________searching first word from an string___________________
+// indexOf will show the first word index____________:
+
+let lyrics = "sada sada kala kala rong jomeche sada kala, hoisi ami mon pagela boshonto kale.";
+let getKala = lyric.indexOf("kala");
+console.log(getKala);
+
+console.log("rong index; ", lyrics.indexOf("rong"));
+
+
+// ______________________startWith___________endWith___________________:
+
+let startWith = lyrics.startsWith("sada");
+console.log("start with sada",startWith);
+
+
+let endWith = lyrics.endsWith("kale.");
+console.log("end with kala", endWith);
+
+
+
+// we can check the file extention with this: 
+
+let dataExtention = "biodata.pdf";
+console.log("file is pdf: ", dataExtention.endsWith(".pdf"));
+
+
+let markSheet = "Jaber-Ahmed-MarkSheet.pdf";
+if(markSheet.endsWith("pdf") === true){
+    console.log("your file is pdf.");
+}
+else{
+    console.log("please choose a pdf file.")
+}
+
+
+
+// ________________split and join_____________________:
+
+let dummyText = "the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox  jumps over the lazy dog. the quick brown fox jumps over the lazy dog.";
+
+let sentenceSplit = dummyText.split('.');
+console.log(sentenceSplit);
+console.log("space split: ",dummyText.split(' '));
+console.log("letter split: ", dummyText.split(''));
+
+
+
+
+
+
+
+// _________________________________________Slice______Slice work with index_________:
